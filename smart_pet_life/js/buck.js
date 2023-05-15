7f9c66fc957f6890a2209f404e034a979d78e530
[wmwwm88] <[wmwwm88@gmail.com]>
2023-05-15 15:37:49 +0900

    -c

---
+++ smart_pet_life/js/buck.js
@@ -0,0 +1,24 @@
+// alert 사용으로 해당 스크립트 미사용
+/* edit by buck : 모달 내 닫기 버튼 -> 모달 닫기*/
+  document.querySelector('.mall-modal button.btn-close-modal').addEventListener('click' , () =>{
+    openMallAlertModal(false);
+  });
+
+  let modal = document.querySelectorAll('.link-to-mall');
+  for(let i = 0; i < modal.length; i++){
+    modal[i].addEventListener("click",openMallAlertModal);
+  }
+
+/* edit by buck */
+function openMallAlertModal( show ){
+  let body =  document.querySelector('body');
+  let targetModal =  document.querySelector('.mall-modal');
+  
+  if(show){ // 모달 오픈
+    targetModal.classList.add('show');
+    body.classList.add('locked'); // 바디 스크롤 잠금
+  } else { // 모달 닫힘
+    targetModal.classList.remove('show');
+    body.classList.remove('locked'); // 바디 스크롤 잠금 해제
+  }
+}
